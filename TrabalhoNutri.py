57b296370f4503f9675b24fb4141f6fc69b927eb
Leonardo Miguel dos Santos <leoms@usp.br>
2023-11-10 12:06:00 -0300

    Update TrabalhoNutri.py
    Adiocionado Função Gato

--- TrabalhoNutri.py
+++ TrabalhoNutri.py
@@ -18,3 +18,15 @@ if Animal == 1:
 
 elif Animal == 2:
     print('Calculando para: Gato')
+    PAGato = int(input('Digite o Peso Atual do animal: '))
+    ECCGato = int(input('Digite o ECC do animal: '))
+    print('TPPS de 0,5 a 1%: {} a {} Kg'.format(PAGato * 0.005, PAGato * 0.01))
+    print('NEPP: {} Kcal'.format(85 * (PAGato) ** 0.4))
+    if ECCGato == 8 or ECCGato == 9:
+        PMGato = PAGato * 0.85
+        print('Peso Meta: {} Kg'.format(PMGato))
+    elif ECCGato == 6 or ECCGato == 7:
+        PMGato = PAGato * 0.90
+        print('Peso Meta: {} Kg'.format(PMGato))
+    else:
+        print('Opção Inválida')
