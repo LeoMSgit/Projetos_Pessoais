7abfa6bff5c473b62ec6fefd688d7791ee3003e0
Leonardo Miguel dos Santos <leoms-98@hotmail.com>
2024-06-04 20:14:38 -0300

    Update Calculadora de Polinômio Geral.py

--- Calculadora de Polinômio Geral.py
+++ Calculadora de Polinômio Geral.py
@@ -9,7 +9,7 @@ def obter_coeficientes():
     grau = int(input("Digite o grau do polinômio: "))
     coeficientes = []
     for i in range(grau + 1):
-        coeficiente = float(input("Digite o coeficiente para x^{}: ".format(grau - i)))
+        coeficiente = float(input(f"Digite o coeficiente para x^{grau - i}: "))
         coeficientes.append(coeficiente)
     return coeficientes
 
@@ -17,9 +17,10 @@ def main():
     print("Bem-vindo à Calculadora de Polinômios")
     print("--------------------------------------")
     coeficientes = obter_coeficientes()
+    coeficiente_x = float(input("Digite o coeficiente para multiplicar x: "))
     x = float(input("Digite o valor de x para calcular o polinômio: "))
-    resultado = calcular_polinomio(coeficientes, x)
-    print("O resultado para x = {} é: {}".format(x, resultado))
+    resultado = calcular_polinomio(coeficientes, x*coeficiente_x)
+    print(f"O resultado para x = {x} é: {resultado}")
 
 if __name__ == "__main__":
     main()
