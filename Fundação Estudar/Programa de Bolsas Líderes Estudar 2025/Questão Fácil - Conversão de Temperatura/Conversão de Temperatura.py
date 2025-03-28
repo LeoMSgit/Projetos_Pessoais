54eb5bcd20324175079b033e91f20242adb4f649
Leonardo Miguel dos Santos <leoms-98@hotmail.com>
2025-03-28 09:42:05 -0300

    Update Conversão de Temperatura.py

--- Fundação Estudar/Programa de Bolsas Líderes Estudar 2025/Questão Fácil - Conversão de Temperatura/Conversão de Temperatura.py
+++ Fundação Estudar/Programa de Bolsas Líderes Estudar 2025/Questão Fácil - Conversão de Temperatura/Conversão de Temperatura.py
@@ -1,13 +1,15 @@
-# your code goes here
 while True:
-    celsius = float(input("Digite a temperatura em Celsius: "))
+    try:
+        celsius = float(input())
 
-    if 0 <= celsius <= 1000:
-        break
-    else:
-        print("Valor inválido! Digite uma temperatura entre 0 e 1000.")
+        if 0 <= celsius <= 1000:
+            break
+        else:
+            print("Valor inválido! Digite uma temperatura entre 0 e 1000.")
+    except ValueError:
+        print("Entrada inválida! Digite um número válido.")
 
 kelvin = celsius + 273.15
 fahrenheit = celsius * 1.80 + 32.00
 
-print(f"{kelvin:.5f}, {fahrenheit:.5f}")
+print("{:.5f}, {:.5f}".format(kelvin, fahrenheit))
